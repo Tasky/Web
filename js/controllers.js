fbb3920fbca3384028f3df5122880e69ac1a5ef2
Nanne Wielinga <nanne@mycel.nl>
2014-03-26 01:48:02 +0100

    begin aan formulier

--- js/controllers.js
+++ js/controllers.js
@@ -1,16 +1,24 @@
 var tasky = angular.module('tasky', ['ngRoute', 'ui.bootstrap'])
     .config(function($routeProvider, $locationProvider) {
         $locationProvider.html5Mode(false);
+    })
+    .config(function($routeProvider) {
         $routeProvider
+
+
+            .when('/projects', {
+                controller: 'ProjectsList',
+                templateUrl: "templates/projects/list.html"
+            })
+
+            .when('/projects/new', {
+                controller: 'ProjectsNew',
+                templateUrl: "templates/projects/edit.html"
+            })
 
             .when('/projects/:id', {
                 controller: 'TaskController',
                 templateUrl: "templates/tasks.html"
-            })
-
-            .when('/projects', {
-                controller: 'ProjectsController',
-                templateUrl: "templates/projects.html"
             })
 
             .otherwise({
--- js/controllers/projects.js
+++ js/controllers/projects.js
@@ -1,8 +1,26 @@
-tasky.controller('ProjectsController', ['$scope', '$routeParams', '$http',
+tasky.controller('ProjectsList', ['$scope', '$routeParams', '$http',
     function($scope, $routeParams, $http) {
         $http.get('http://tasky.nl/api/projects').success(function(data) {
-            console.log(data);
             $scope.projects = data;
-//            $scope.mainImageUrl = data.images[0];
         });
+    }]);
+
+tasky.controller('ProjectsNew', ['$scope', '$routeParams', '$http',
+    function($scope, $routeParams, $http) {
+        $scope.action = 'Aanmaken';
+
+        $scope.save = function() {
+            if($scope.projectName == "") {
+                return;
+            }
+
+            $http.post('http://tasky.nl/api/projects', {
+                name: $scope.projectName
+            }).success(function(data) {
+
+            }).error(function(data) {
+                    // TODO: error handling
+                    alert("huilie :'( \n"+data)
+                });
+        };
     }]);
