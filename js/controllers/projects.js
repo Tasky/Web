b227b3cba3f102b59ec2d023062604202de0f06f
Nanne Wielinga <nanne@mycel.nl>
2014-03-27 16:09:12 +0100

    verwijderen van projecten (omdat het handig is)

--- js/controllers/projects.js
+++ js/controllers/projects.js
@@ -2,7 +2,17 @@ tasky.controller('ProjectsList', ['$scope', '$routeParams', '$http',
     function($scope, $routeParams, $http) {
         $http.get('http://tasky.nl/api/projects').success(function(data) {
             $scope.projects = data;
+            console.log(data);
         });
+
+        $scope.delete = function(id) {
+            if(confirm("Weet je dit zeker?")){
+                $http.delete('http://tasky.nl/api/project/'+id).success(function(data) {
+                    window.location = '#/projects/';
+                });
+            }
+        };
+
     }]);
 
 tasky.controller('ProjectsNew', ['$scope', '$routeParams', '$http',
@@ -10,17 +20,17 @@ tasky.controller('ProjectsNew', ['$scope', '$routeParams', '$http',
         $scope.action = 'Aanmaken';
 
         $scope.save = function() {
-            if($scope.projectName == "") {
+            if($scope.projectName == null || $scope.projectName == "") {
                 return;
             }
 
-            $http.post('http://tasky.nl/api/projects', {
+            $http.post('http://tasky.nl/api/project', {
                 name: $scope.projectName
             }).success(function(data) {
-
+                window.location = '#/projects/'+data;
             }).error(function(data) {
-                    // TODO: error handling
-                    alert("huilie :'( \n"+data)
-                });
+                // TODO: error handling
+                alert("huilie :'( \n"+data)
+            });
         };
     }]);
