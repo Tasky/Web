f4eb0566b6069b301f7a9f08109ce99649ced5fd
Nanne Wielinga <nanne@mycel.nl>
2014-03-30 14:43:57 +0200

    stuk denk ik, stomme tijden

--- js/controllers/activities.js
+++ js/controllers/activities.js
@@ -2,7 +2,7 @@ tasky.controller('Activities', ['$scope', '$routeParams', '$http', '$interval', 
     $scope.name = "Activities";
     $scope.params = $routeParams;
 
-    
+
     var started = false;
     // http://tasky.nl/api/project/3
     $scope.action = 'play';
@@ -13,7 +13,7 @@ tasky.controller('Activities', ['$scope', '$routeParams', '$http', '$interval', 
 
     var calculateTime = function() {
         function padLeft(nr, n, str){
-            return Array(n-String(nr).length+1).join(str||'0')+nr;
+            return new Array(n-String(nr).length+1).join(str||'0')+nr;
         }
 
         var hour = Math.floor(sec / (60*60));
@@ -21,6 +21,40 @@ tasky.controller('Activities', ['$scope', '$routeParams', '$http', '$interval', 
         $scope.currenttime = padLeft(hour,2) + ":" + padLeft(min, 2) + ":" + padLeft(sec % 60, 2);
     };
 
+    $scope.edit = function() {
+
+        var diff = $scope.stoptime - $scope.starttime;
+
+        sec = Math.abs(diff / 1000);
+        console.log(sec);
+
+        calculateTime();
+    };
+
+    $scope.save = function() {
+        alert('afsddsaf');
+    };
+
+    $scope.timer = function() {
+        var today=new Date();
+        if($scope.action == 'play') {
+            if(!started) {
+                $scope.starttime = today;
+                started = true;
+            }
+            $scope.action = 'pause';
+
+            ival = $interval(function() {
+                sec++;
+                calculateTime();
+            }, 1000);
+
+        } else {
+            $interval.cancel(ival);
+            $scope.stoptime = today;
+            $scope.action = 'play';
+        }
+    };
 
     calculateTime();
 
@@ -31,26 +65,5 @@ tasky.controller('Activities', ['$scope', '$routeParams', '$http', '$interval', 
 
     $http.get(url+'/project/'+$routeParams.id+'/task/'+$routeParams.taskid).success(function(data) {
         $scope.task = data;
-
-        $scope.timer = function() {
-            var today=new Date();
-            if($scope.action == 'play') {
-                if(!started) {
-                    $scope.starttime = today;
-                    started = true;
-                }
-                $scope.action = 'pause';
-
-                ival = $interval(function() {
-                    sec++;
-                    calculateTime();
-                }, 1000);
-
-            } else {
-                $interval.cancel(ival);
-                $scope.stoptime = today;
-                $scope.action = 'play';
-            }
-        };
     });
 }]);
